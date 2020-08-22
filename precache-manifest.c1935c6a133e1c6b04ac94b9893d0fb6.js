96381dcad218cb186d18e2f66303db5c18ff3dd5
Matteo Cigognini <cigognini.matteo@gmail.com>
2020-08-23 01:54:19 +0200

    Updates

---
+++ precache-manifest.c1935c6a133e1c6b04ac94b9893d0fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d30cc396f5347c566ec54f90715380",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8875a2148af6281decc",
+    "url": "/static/css/main.12c600bb.chunk.css"
+  },
+  {
+    "revision": "f9db9fe2c2429df828f3",
+    "url": "/static/js/2.ac9a2ec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ac9a2ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8875a2148af6281decc",
+    "url": "/static/js/main.7149f6ab.chunk.js"
+  },
+  {
+    "revision": "3ed0d82e38f20ac55534",
+    "url": "/static/js/runtime-main.d60effc5.js"
+  },
+  {
+    "revision": "1608ea93be274312f33b3747d698067d",
+    "url": "/static/media/coding_six_monitors.1608ea93.svg"
+  }
+]);
